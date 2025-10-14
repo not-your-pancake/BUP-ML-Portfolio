4d1896af76b75fc0fb77cf4e3950dae88c34064e
Khalid <nabilhasan01541@gmail.com>
2025-10-14 21:54:07 +0600

    traverse

---
+++ 09-uni-data-structure/lab-3-array/traversing.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5] = {2,4,5,6,7};
+    int i=0;
+    while(i<5){
+        cout << arr[i] << " ";
+        i++; 
+    }
+
+    return 0;
+}
