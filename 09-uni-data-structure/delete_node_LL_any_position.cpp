9a230e897699ffe268477e024b7a6bd5c5fab3fc
khalid from laptop <nabilhasan01541@gmail.com>
2025-11-23 11:39:27 +0600

    linked list deletion updated

--- 09-uni-data-structure/delete_node_LL_any_position.cpp
+++ 09-uni-data-structure/delete_node_LL_any_position.cpp
@@ -1,61 +1,80 @@
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 struct Node{
-    int data;
-    Node* next;
+  int data;
+  Node* next;
 };
 
-void display(Node*  head){
-    while (head != NULL) {
+void display(Node *head){
+    while ( head != NULL){
         cout << head->data << " ";
         head = head->next;
     }
+    cout << "\n\n";
 }
-int main() {
+
+int main(){
     
-    //deletes a node from any position in a linked list.
-    
-    Node *n1, *n2, *n3;
+    Node *n1, *n2, *n3, *n4;
     
     n1 = new Node();
     n2 = new Node();
     n3 = new Node();
+    n4 = new Node();
     
-    n1 -> data = 20;
-    n1 -> next = n2;
+    n1-> data = 15;
+    n1-> next = n2;
     
-    n2 -> data = 25;
-    n2 -> next = n3;
+    n2-> data = 25;
+    n2-> next = n3;
     
-    n3 -> data = 30;
-    n3 -> next = NULL;
+    n3-> data = 35;
+    n3-> next = n4;
+    
+    n4-> data = 45;
+    n4-> next = NULL;
     
     Node *head;
     head = n1;
-    int position;
-    cin >> position;
-    if (position == 0) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    } else {
-        Node* current = head;
-        for (int i = 0; current != NULL && i < position - 1; i++) {
-            current = current->next;
-        }
-        if (current == NULL || current->next == NULL) {
-            cout << "Position out of bounds" << endl;
-        } else {
-            Node* temp = current->next;
-            current->next = temp->next;
-            delete temp;
-        }
-    }    
+    
+    Node *curr;
+    curr = head;
+    
+    Node *prev;
+    prev = NULL;
+    
+    // before any change
     
     display(head);
-    cout << endl;
-
+    
+    // // val to delete 
+    
+    // int val_to_delete = 15;
+    
+    // if(curr != NULL && curr->data == val_to_delete){
+    //     head = head->next;
+    //     delete curr;
+    // }
+    
+    // // after deletion from n1
+    // display(head);
+    
+    /// deleteing at any point
+    int val_to_delete = 35;
+    
+    while(curr != NULL && curr->data != val_to_delete){
+        prev = curr;
+        curr = curr->next;
+    }
+    
+    if( curr != NULL){
+        prev->next = curr->next;
+        delete curr;
+    }
+    
+    // delete at any position
+    display(head);
+    
     return 0;
 }
