e893dbfa8cf71bff9ade740f635a28828edeae11
Khalid <nabilhasan01541@gmail.com>
2026-03-30 00:04:38 +0600

    bst insertion

--- 09-uni-data-structure/algorith and design course/BST/bst_insertion.cpp
+++ 09-uni-data-structure/algorith and design course/BST/bst_insertion.cpp
@@ -1,33 +1,65 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
+// Structure for a BST Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
 
-struct Node{
-
-int data;
-Node* left;
-Node* right;
-
-Node(int val){
-
-data = val;
-left = nullptr;
-right = nullptr;
-
-}
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
-Node
+// Function to insert a value into the BST
+Node* insert(Node* root, int val) {
+    // If the tree is empty, return a new node
+    if (root == nullptr) {
+        return new Node(val);
+    }
 
+    // Otherwise, recur down the tree
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } else if (val > root->data) {
+        root->right = insert(root->right, val);
+    }
 
+    // Return the (unchanged) node pointer
+    return root;
+}
 
-int main(){
+// Inorder Traversal: Left -> Root -> Right
+void inorder(Node* root) {
+    if (root == nullptr) return;
 
+    inorder(root->left);       // Visit Left
+    cout << root->data << " "; // Visit Root
+    inorder(root->right);      // Visit Right
+}
 
+int main() {
+    Node* root = nullptr;
+    int n, value;
 
+    cout << "Enter the number of integers (N): ";
+    cin >> n;
 
+    cout << "Enter " << n << " integers:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> value;
+        root = insert(root, value);
+    }
 
-return 0;
+    cout << "\nInorder Traversal (Sorted Elements):\n";
+    inorder(root);
+    cout << endl;
+
+    return 0;
 }
