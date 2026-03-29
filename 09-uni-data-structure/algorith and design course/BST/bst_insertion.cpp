b56ac0c8064b3e40f6e520e9dc1d67df712374ab
Khalid <nabilhasan01541@gmail.com>
2026-03-29 23:37:23 +0600

    uni dSA BST

---
+++ 09-uni-data-structure/algorith and design course/BST/bst_insertion.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+struct Node{
+
+int data;
+Node* left;
+Node* right;
+
+Node(int val){
+
+data = val;
+left = nullptr;
+right = nullptr;
+
+}
+};
+
+Node
+
+
+
+int main(){
+
+
+
+
+
+return 0;
+}
