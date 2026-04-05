055fecd0672d37ecf68f2b24cd2274e4c5a132bc
Khalid <nabilhasan01541@gmail.com>
2026-04-05 19:50:15 +0600

    merge sort updated

--- 09-uni-data-structure/algorith and design course/lab2_merge_sort/merges_sort.cpp
+++ 09-uni-data-structure/algorith and design course/lab2_merge_sort/merges_sort.cpp
@@ -1,26 +1,72 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+
 using namespace std;
 
-void merge_sort(int arr[], int lb, int ub){
-    if (lb<ub){
-        int mid = (lb + ub) / 2;
-        merge_sort(arr, lb, mid);
-        merge_sort(arr, mid+1, ub);
-        merge(arr,lb,mid,ub);
-    }
+void merge(vector<int>& arr, int start, int mid, int end) {
+	vector<int> temp;
+	int i = start;
+	int j = mid+1;
+
+	// suppose euqal number of left and right
+	while(i<= mid and j<= end) {
+		if(arr[i]<= arr[j]) {
+			temp.push_back(arr[i]);
+			i++;
+		}
+		else {
+			temp.push_back(arr[j]);
+			j++;
+		}
+	} // end of left = right
+
+	//if one side is more than the other
+	while(i<=mid) {
+		temp.push_back(arr[i]);
+		i++;
+	}
+
+	while(j<= end) {
+		temp.push_back(arr[j]);
+		j++;
+	}
+
+	// updatng the orginal array from the temp
+
+	for(int idx=0; idx<temp.size(); idx++) {
+		arr[start+idx] = temp[idx];
+	}
 }
 
-void merge(int arr[], int lb, int mid, int ub){
-    int i = lb;
-    int j = mid + 1;
-    int k = lb;
+void mergeSort(vector<int>& arr, int start,  int end) {
+	if(start<end) {
+		int mid;
+		mid = (start+end)/2;
+		mergeSort(arr,start, mid);
+		mergeSort(arr,mid+1,end);
+
+		merge(arr,start,mid,end);
+	}
+
+
 }
 
-int main(){
+void display(vector<int>& arr, int n) {
+	for(int i=0; i<n; i++) {
+		cout<< arr[i] <<" ";
+	}
+	cout <<"\n";
+}
+int main() {
 
-    
+	vector<int> arr = {12,31,35,8,32,17};
+	int n = arr.size();
+
+	display(arr,n);
+
+	mergeSort(arr,0,n-1);
+	display(arr,n);
 
 
-
-    return 0;
+	return 0;
 }
