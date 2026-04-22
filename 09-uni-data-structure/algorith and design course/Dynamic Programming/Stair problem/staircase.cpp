937c8c661d9d7c73dd1b2b569414598dcc70aa8a
Khalid <nabilhasan01541@gmail.com>
2026-04-22 11:39:18 +0600

    staircase and fibbonachi

--- 09-uni-data-structure/algorith and design course/Dynamic Programming/Stair problem/staircase.cpp
+++ 09-uni-data-structure/algorith and design course/Dynamic Programming/Stair problem/staircase.cpp
@@ -3,12 +3,33 @@
 #include <string>
 #include <algorithm>
 
-
 using namespace std;
+
+int ways(int n){
+    vector<int> notebook(n+1,0);
+
+    notebook[0] = 1;
+    notebook[1] = 1;
+
+    for(int i=2; i<=n; i++){
+        notebook[i] = notebook[i-1] + notebook[i-2];
+    }
+
+    cout << notebook[n];
+}
 
 int main(){
 
+    // A person is climbing a staircase with n steps. They can climb either 1 step or 2 steps at a time. Find the number of distinct ways to reach the top.
 
+    int stairs;
 
-return 0;
+    cout << "enter number of staris: \n";
+    cin >> stairs;
+
+    // stairs = 5;
+
+    ways(stairs);
+
+    return 0;
 }
